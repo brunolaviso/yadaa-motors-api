a669fd7a6007f53aceb5013361a92e8bc56573b5
Bruno Laviso <brunolaviso@gmail.com>
2023-12-13 22:21:18 -0300

    feat: implementa db

--- index.js
+++ index.js
@@ -1,80 +1,79 @@
 const express = require('express')
-// import express from 'express'
 const { v4: uuid } = require('uuid')
+const { PrismaClient } = require('@prisma/client')
 
 const app = express()
 
 app.use(express.json())
 
-const cars = [
-  {
-    id: uuid(),
-    name: 'Fusca'
-  },
-  {
-    id: uuid(),
-    name: 'Brasília'
-  },
-  {
-    id: uuid(),
-    name: 'Chevette'
-  }
-]
+const prisma = new PrismaClient()
 
-app.get('/cars', (request, response) => {
+app.get('/cars', async (request, response) => {
+  const cars = await prisma.cars.findMany()
   return response.status(200).json(cars)
 })
 
-app.get('/cars/:id', (request, response) => {
+app.get('/cars/:id', async (request, response) => {
   const { id } = request.params
 
-  const car = cars.find(car => car.id === id)
+  const car = await prisma.cars.findUnique({
+    where: {
+      id
+    }
+  })
 
   if (!car) return response.status(404).json({ error: 'Car not found' })
 
   return response.status(200).json(car)
 })
 
-app.post('/cars', (request, response) => {
+app.post('/cars', async (request, response) => {
   const { name } = request.body
 
   if (!name) return response.status(400).json({ error: 'Name is required' })
 
-  const car = {
-    id: uuid(),
-    name
-  }
-
-  cars.push(car)
+  const car = await prisma.cars.create({
+    data: {
+      id: uuid(),
+      name
+    }
+  })
 
   return response.status(201).json(car)
 })
 
-app.put('/cars/:id', (request, response) => {
+app.put('/cars/:id', async (request, response) => {
   const { id } = request.params
   const { name } = request.body
 
   if (!name) return response.status(400).json({ error: 'Name is required' })
 
-  const car = cars.find(car => car.id === id)
+  const car = await prisma.cars.update({
+    where: {
+      id
+    },
+    data: {
+      name
+    }
+  })
 
   if (!car) return response.status(404).json({ error: 'Car not found' })
-
-  car.name = name
 
   return response.status(200).json(car)
 })
 
-app.delete('/cars/:id', (request, response) => {
+app.delete('/cars/:id', async (request, response) => {
   const { id } = request.params
 
-  const carIndex = cars.findIndex(car => car.id === id)
+  const car = await prisma.cars.delete({
+    where: {
+      id
+    }
+  })
 
-  if (carIndex < 0) return response.status(404).json({ error: 'Car not found' })
+  if (!car) return response.status(404).json({ error: 'Car not found' })
 
-  cars.splice(carIndex, 1)
-
-  return response.status(200).json(cars)
+  return response.status(204).json()
 })
 
 app.listen(3333, () => console.log('Server is running'))
---
+++ prisma/migrations/20231207010229_init/migration.sql
@@ -0,0 +1,5 @@
+-- CreateTable
+CREATE TABLE "Cars" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL
+);
